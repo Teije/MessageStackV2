a3dd2008952114dd30ae206b430ddd4225d74ca4
teijevhemert@gmail.com <teijevhemert@gmail.com>
2020-01-31 08:05:44 +0100

    Home index now redirects when logged in

--- MessageStack/MessageStack/Controllers/HomeController.cs
+++ MessageStack/MessageStack/Controllers/HomeController.cs
@@ -1,12 +1,23 @@
 ﻿using System.Web.Mvc;
+using MessageStack.Context;
 
 namespace MessageStack.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
+        public HomeController() : this (new MessageStackContext())
+        {
+        }
+        public HomeController(MessageStackContext messageStackContext) : base(messageStackContext)
+        {
+        }
+
         public ActionResult Index()
         {
+            if (IsLoggedIn()) return RedirectToAction("Index", "Account");
+
             return View();
         }
+
     }
 }
